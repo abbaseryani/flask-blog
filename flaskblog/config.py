d2567c331ba469d5645bc784b9ff6154e8095a93
Eryani <abbaseryani@gmail.com>
2018-11-08 16:13:01 +0100

    Flask Blog

---
+++ flaskblog/config.py
@@ -0,0 +1,8 @@
+class Config:
+    SECRET_KEY = '965f53c1e5a6ad0410ac27d9861bea82'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'eryani.app@gmail.com'
+    MAIL_PASSWORD = 'Blois_123'
